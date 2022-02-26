acb0bb1824d83a1c243e3665f7803ccf40b0fdd2
frc95 <frc95zz@gmail.com>
2022-02-26 17:32:44 -0300

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from django.urls import path
+from .views import render_post, post_detail
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', render_post, name='posts'),
+    path('/<int:post_id>', post_detail, name="post_detail")
+]
